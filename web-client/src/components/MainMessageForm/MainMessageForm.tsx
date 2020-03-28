34373b23225e0822d5696a4121250c7d77ea0fc3
Marica Bertarini <maricabertarini@gmail.com>
2020-03-28 12:37:14 +0100

    very basic UI of the main message form display content from a sample Template instance

--- web-client/src/components/MainMessageForm/MainMessageForm.tsx
+++ web-client/src/components/MainMessageForm/MainMessageForm.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './MainMessageForm.scss';
+import { Template } from '../../model/model';
 
-const MainMessageForm: React.FC = () => (
-  <div className="MainMessageForm">
-    MainMessageForm Component
-  </div>
-);
+type MainMessageFormProps = {
+  template: Template
+}
+
+const MainMessageForm = (props: MainMessageFormProps) => {
+  let menuListItems = props.template.menuItems.map(menuItem => 
+    <div>{menuItem.index}. {menuItem.title}</div>);
+
+  return (
+    <div className="MainMessageForm">
+      <h4>Header content</h4>
+      <textarea rows={8} defaultValue={props.template.header}>
+      </textarea>
+
+      <h4>Menu</h4>
+      <div>
+        {menuListItems}
+      </div>
+    </div>
+  );
+};
 
 export default MainMessageForm;
