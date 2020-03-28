ade823ebad8a7d689be4146d264dcb0b2290c6c8
Marica Bertarini <maricabertarini@gmail.com>
2020-03-28 11:40:55 +0100

    added empty MainMessageForm component

---
+++ web-client/src/components/MainMessageForm/MainMessageForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MainMessageForm.scss';
+
+const MainMessageForm: React.FC = () => (
+  <div className="MainMessageForm">
+    MainMessageForm Component
+  </div>
+);
+
+export default MainMessageForm;
