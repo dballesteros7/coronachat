307e88d56a8ae3ace3a5f5135412a05d75875465
Diego Balelsteros <dballesteros7@gmail.com>
2020-03-28 02:22:51 +0100

    Test only

--- data/schema_test.py
+++ data/schema_test.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from schema import Base
 
 def main():
+  print("Testing")
   engine = create_engine('sqlite:///:memory:', echo=True)
   Base.metadata.create_all(engine)
 
