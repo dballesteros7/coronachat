4580677df4d0cbe3834da10958e6cc679b45d155
Marica Bertarini <maricabertarini@gmail.com>
2020-03-30 22:57:53 +0200

    changed main content in home

--- web-client/src/pages/Home/Home.tsx
+++ web-client/src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import logo from '../../assets/images/coronachat-logo.svg';
 import { Button, makeStyles, Theme, createStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import MessagePreview from '../../components/MessagePreview/MessagePreview';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,15 +20,35 @@ const Home = () => {
   return (
     <div className="Home">
       <img id="logo" src={logo} alt="Coronainfochat"/>
-      <h1 className="covid-title">Stop misinformation about COVID-19 and inform your people officially</h1>
-      <p id="main-content">
-        You are a government, an institution, or an organisation<br />
-        You want the community to access your official information via <b>WhatsApp</b><br /><br />
+      <h2 className="covid-title">Stop misinformation about COVID-19 and inform your people officially</h2>
 
-        Our tool sets up the infrastracture and creates a chatbot for your<br /><br />
-
-        You just need to seed it with the content of the messages that the people will receive<br />
-      </p>
+      <div id="chat-box">
+        <div id="message-box-1" className="message-box">
+          <MessagePreview value="Hello. I am the mayor of a South American town. The people here use *WhatsApp* a lot and 
+            share many chains and fake news about COVID-19."/>
+        </div>
+        <div id="message-box-2" className="message-box">
+          <MessagePreview value="They also have limited data plans and access to important
+            information is not always possible. I saw that WHO and India have some phone number that people can write to
+            via Whatsapp and get *official information quickly* via message..."/>
+        </div>
+        <div id="message-box-3" className="message-box">
+          <MessagePreview value="I don't know how to configure one for the people here."/>
+        </div>
+        <div id="message-box-4" className="message-box">
+          <MessagePreview value="Hello Mr Gonzales, we appreciate your initiative and would like to help you out."/>
+        </div>
+        <div id="message-box-5" className="message-box">
+          <MessagePreview value="Our app _Coronainfochat_ creates the chatbot for you. You just need to feed it with the
+            content that the people of your town will read in the chat. It works with *Facebook Messenger* too."/>
+        </div>
+        <div id="message-box-6" className="message-box">
+          <MessagePreview value="Awesome! Thank you. Can they also get a notification with important updates?"/>
+        </div>
+        <div id="message-box-7" className="message-box">
+          <MessagePreview value="Not yet, but soon. We'll keep you posted. Try the app by clicking below and stay _healhty_!"/>
+        </div>
+      </div>
       <Link to="/dashboard">
         <Button className={classes.startButton} size="small" 
           variant="contained" color="primary">
