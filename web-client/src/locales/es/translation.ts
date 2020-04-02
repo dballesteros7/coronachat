277f156e6704b2fbe4e0caf33076b7c931697607
Diego Balelsteros <dballesteros7@gmail.com>
2020-04-03 00:32:45 +0200

    Add spanish translations

--- web-client/src/locales/es/translation.ts
+++ web-client/src/locales/es/translation.ts
@@ -3,7 +3,7 @@ const translationES = {
   MSG_HEADER: 'Encabezado del mensaje',
   MSG_HEADER_PLACEHOLDER:
     'Escriba aquí una descripción personalizada sobre este servicio o pulse "Rellenar" para obtener un mensaje estándar.',
-  CONTENT: 'Content',
+  CONTENT: 'Contenido',
   TITLE_CANT_BE_EMPTY: 'El título no puede estar vacío.',
   CONTENT_CANT_BE_EMPTY: 'El contenido no puede estar vacío.',
   UNSAVED_CHANGES_DIALOG: {
@@ -25,16 +25,28 @@ const translationES = {
     OPTION_TITLE: 'Título de la opción (visible en el menú principal)',
     OPTION_TITLE_PLACEHOLDER: 'Escribe el texto que se ve en la opción del menu principal.',
     OPTION_CONTENT_PLACEHOLDER: 'Escriba aquí el contenido del mensaje enviado cuando se selecciona esta opción.',
-    DELETE_OPTION: 'Borrar esta opciòn',
+    DELETE_OPTION: 'Borrar esta opción',
     DELETE_OPTION_DIALOG: {
-      TITLE: 'Borrar esta opciòn',
+      TITLE: 'Borrar esta opción',
       MESSAGE: `Si borra esta opción, se eliminará del menú principal y se perderá su contenido. ¿Está seguro?`,
       YES_BUTTON: 'Sí, borrar la opción',
       NO_BUTTON: 'No borrarla',
     },
   },
   HOME: {
-    // TODO Please Diego add spanish translations here (copy structre from en/translations.ts)
+    HEADER: 'Detenga la desinformación acerca de COVID-19 e informe sus comunidades de manera oficial',
+    CHAT: {
+      MESSAGE_1: `Hola, soy el alcalde de una ciudad pequeña. Los ciudadanos aquí usan mucho *WhatsApp*  comparte muchas cadenas con noticias e información falsa sobre COVID-19.🦠`,
+      MESSAGE_2: `La gente de nuestra comunidad tiene planes de datos limitados y no siempre es posible acceder a sitios web. He visto que la OMS y el gobierno Indio tienen números telefónicos donde la gente puede escribir por WhatsApp para recibir *información oficial de manera rápida* con mensajes de texto...`,
+      MESSAGE_3: `No sé cómo configurar algo así aquí`,
+      MESSAGE_4: `Hola Sr. Gonzales, apreciamos su iniciativa y nos gustaría ayudarle.`,
+      MESSAGE_5: `Nuestra aplicación _Coronainfochat_ crea el chatbot por usted. Usted sólo necesita llenarlo con el contenido que las personas en su ciudad van a leer en el chat. También funciona con Facebook Messenger*.`,
+      MESSAGE_6: `Excelente! Muchas gracias. Ellos pueden recibir también una notificación con actualizaciones importantes?`,
+      MESSAGE_7: `Todavía no, pero pronto. Lo mantendremos al tanto. Por favor pruebe la aplicación y mantengáse _sano_! 💪🏼`,
+      MESSAGE_8: `Cuánto va a costar? El presupuesto está apretado 💰😐`,
+      MESSAGE_9: `Nada, es *gratis* por ahora. Puede que tenga que cubrir los costos de infraestructura en el futuro. No dude en contactarnos con cualquier pregunta! 👇🏼`,
+    },
+    TRY_IT_BUTTON: `Pruébelo ahora`,
   },
 };
 
