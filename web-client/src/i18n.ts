80076cc1a56068cefd51a46a4b36b7a716b6a262
Marica Bertarini <maricabertarini@gmail.com>
2020-04-02 12:53:06 +0200

    converted es translation files to ts

--- web-client/src/i18n.ts
+++ web-client/src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import translationEN from './locales/en/translation';
-import translationES from './locales/es/translation.json';
+import translationES from './locales/es/translation';
 
 const resources = {
   en: {
