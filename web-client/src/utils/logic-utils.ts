a3b8492190220211cb30abcd46ed3046d4828c9b
Marica Bertarini <maricabertarini@gmail.com>
2020-04-20 18:14:42 +0200

    adapted getLocalDefaultTemplateForLanguage return type to ReadOnlyTemplate (defaultTemplate shouldn't be modified anywhere in the app)

--- web-client/src/utils/logic-utils.ts
+++ web-client/src/utils/logic-utils.ts
@@ -1,10 +1,10 @@
 import { Language, Languages } from '../i18n';
-import { Template } from '../model/model';
+import { ReadOnlyTemplate } from '../model/model';
 import { defaultTemplateEN } from '../locales/en/defaultTemplate';
 import { defaultTemplateES } from '../locales/es/defaultTemplate';
 import { defaultTemplateIT } from '../locales/it/defaultTemplate';
 
-export function getLocalDefaultTemplateForLanguage(language: Language): Template {
+export function getLocalDefaultTemplateForLanguage(language: Language): ReadOnlyTemplate {
   switch (language) {
     case Languages.es:
       return defaultTemplateES;
