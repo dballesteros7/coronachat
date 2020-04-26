dacdd465c077b866c944fb61fc3f8baef7a84028
Marica Bertarini <maricabertarini@gmail.com>
2020-04-26 17:23:50 +0200

    fixed useLocalStorage to support undefined default value

--- web-client/src/hooks/useLocalStorage.ts
+++ web-client/src/hooks/useLocalStorage.ts
@@ -1,8 +1,15 @@
 import { useState } from 'react';
 
-const useLocalStorage = <T>(key: string, defaultState: T): [T, (newValue?: T) => void] => {
-  const initState = () => JSON.parse(window.localStorage.getItem(key) || JSON.stringify(defaultState)) as T;
-  const [storageValue, setStorageInState] = useState(initState);
+const useLocalStorage = <T>(key: string, defaultState?: T): [T | undefined, (newValue?: T) => void] => {
+  const initState = () => {
+    const storedState = window.localStorage.getItem(key);
+    if (storedState) {
+      return JSON.parse(storedState) as T;
+    } else {
+      return defaultState ? (JSON.parse(JSON.stringify(defaultState)) as T) : undefined;
+    }
+  };
+  const [storageValue, setStorageInState] = useState<T | undefined>(initState);
   const setStorageValue = (newValue?: T) => {
     setStorageInState(newValue ?? defaultState);
     newValue ? window.localStorage.setItem(key, JSON.stringify(newValue)) : window.localStorage.removeItem(key);
